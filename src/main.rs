8785ea0f9e3519eb71b932795a23fe4ae71e0935
fabrisdev <ezepuntocom@hotmail.com>
2026-04-13 07:15:37 -0300

    install dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use std::process::Command;
 
 const PROJECT_GIT_URL: &str = "https://github.com/Tu-vecino-digital/tvd-web";
+const PROJECT_FOLDER_NAME: &str = "tvd-web";
 
 fn main() {
     clone_project();
+    install_dependencies();
 }
 
 fn clone_project() {
-    let mut child = Command::new("git")
+    let status = Command::new("git")
         .arg("clone")
         .arg(PROJECT_GIT_URL)
-        .spawn()
+        .status()
         .expect("Failed launching git. Do you have it installed?");
-    child.wait().expect("Failed waiting for git to exit.");
+    assert!(status.success(), "Failed cloning repository.");
+}
+
+fn install_dependencies() {
+    let status = Command::new("bun")
+        .current_dir(PROJECT_FOLDER_NAME)
+        .arg("install")
+        .status()
+        .expect("Failed launching bun. Do you have it installed?");
+    assert!(status.success(), "Failed during dependency installation.");
 }
