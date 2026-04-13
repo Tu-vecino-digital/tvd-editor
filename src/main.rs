23257ca234a6b0c1a239e3e63ae8a35dca96aa29
fabrisdev <ezepuntocom@hotmail.com>
2026-04-13 08:04:39 -0300

    install project inside documents folder

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
-use std::{fs, process::Command};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+use directories::UserDirs;
 
 const PROJECT_GIT_URL: &str = "https://github.com/Tu-vecino-digital/tvd-web";
 const PROJECT_FOLDER_NAME: &str = "tvd-web";
 
 fn main() {
-    if project_already_exists() {
-        update_project();
+    let user_dirs = UserDirs::new().unwrap();
+    let document_dir = user_dirs.document_dir().unwrap();
+    let project_path = document_dir.join(PROJECT_FOLDER_NAME);
+
+    if project_already_exists(&project_path) {
+        update_project(&project_path);
     } else {
-        clone_project();
+        clone_project(document_dir);
     }
-    install_dependencies();
-    start_project();
+    install_dependencies(&project_path);
+    start_project(&project_path);
 }
 
-fn project_already_exists() -> bool {
-    fs::exists(PROJECT_FOLDER_NAME).expect(
+fn project_already_exists(project_path: &PathBuf) -> bool {
+    fs::exists(project_path).expect(
         "Failed checking if project folder already exists. Could it be missing permissions?",
     )
 }
 
-fn update_project() {
+fn update_project(project_path: &PathBuf) {
     let status = Command::new("git")
-        .current_dir(PROJECT_FOLDER_NAME)
+        .current_dir(project_path)
         .arg("pull")
         .status()
         .expect("Failed launching git. Do you have it installed?");
     assert!(status.success(), "Failed updating project.");
 }
 
-fn clone_project() {
+fn clone_project(installation_path: &Path) {
     let status = Command::new("git")
+        .current_dir(installation_path)
         .arg("clone")
         .arg(PROJECT_GIT_URL)
         .status()
@@ -37,18 +48,18 @@ fn clone_project() {
     assert!(status.success(), "Failed cloning repository.");
 }
 
-fn install_dependencies() {
+fn install_dependencies(project_path: &PathBuf) {
     let status = Command::new("bun")
-        .current_dir(PROJECT_FOLDER_NAME)
+        .current_dir(project_path)
         .arg("install")
         .status()
         .expect("Failed launching bun. Do you have it installed?");
     assert!(status.success(), "Failed during dependency installation.");
 }
 
-fn start_project() {
+fn start_project(project_path: &PathBuf) {
     Command::new("bun")
-        .current_dir(PROJECT_FOLDER_NAME)
+        .current_dir(project_path)
         .arg("run")
         .arg("dev")
         .status()
